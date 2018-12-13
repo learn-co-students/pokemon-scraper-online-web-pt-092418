6e441d605ed10bd1888ce8ce67a15c8755bceb2c
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-13 12:36:49 -0500

    Interim checkin

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+attr_accessor :id, :name, :type, :db
+  @@all = {}
+def initialize(id)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(id, name, type)
+   self.class.all << self
+  end
+
+  def find
+  end
+
 end
